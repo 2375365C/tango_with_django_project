56bb0970f40cc20ed54f7929448f8d459fb1c3d8
Charles Cameron <2375365C@student.gla.ac.uk>
2020-01-31 02:27:48 +0000

    TWD completed exercises page 40

---
+++ rango/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+	return HttpResponse("<a href='about'>Rango says hey there partner!<a>")
+
+def about(request):
+	return HttpResponse("<a href='..'>Rango says here is the about page.</a>")
