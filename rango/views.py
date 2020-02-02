a90b99a1f42ba49d31d3666c6f4ec420a8f49965
Charles Cameron <2375365C@student.gla.ac.uk>
2020-02-02 00:24:49 +0000

    passes chapter 6&7 tests

--- rango/views.py
+++ rango/views.py
@@ -10,9 +10,11 @@ def index(request):
 	# Order the categories by the number of likes in descending order.
 	# Retrieve the top 5 only (or all if less than 5).
 	category_list = Category.objects.order_by('-likes')[:5]
+	pages_list = Page.objects.order_by('-views')[:5]
 	context_dict = {}
 	context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
 	context_dict['categories'] = category_list
+	context_dict['pages'] = pages_list
 	# Render the response and send it back!
 	return render(request, 'rango/index.html', context=context_dict)
 
@@ -24,12 +26,11 @@ def show_category(request, category_name_slug):
 	try:
 		# Can we find a category name slug with the given name?
 		# If we can't, the .get() method raises a DoesNotExist exception.
-		category = Category.objects.get(slug=category_name_slug) # Retrieve all of the associated pages.
+		category = Category.objects.get(slug=category_name_slug) 
+		# Retrieve all of the associated pages.
 		pages = Page.objects.filter(category=category) 
+		
 		context_dict['pages'] = pages # Adds our results list to the template context under name pages.
-		# We also add the category object from
-		# the database to the context dictionary.
-		# We'll use this in the template to verify that the category exists.
 		context_dict['category'] = category
 
 	except Category.DoesNotExist:
@@ -84,6 +85,6 @@ def add_page(request, category_name_slug):
 		
 		else:
 			print(form.errors)
-			
+
 	context_dict = {'form': form, 'category': category}
 	return render(request, 'rango/add_page.html', context=context_dict)
