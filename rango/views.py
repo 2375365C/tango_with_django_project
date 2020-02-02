d6e7a314668b861328852621820eaa8f9088ba4d
Charles Cameron <2375365C@student.gla.ac.uk>
2020-02-02 12:44:43 +0000

    Completed chapter 9 tests

--- rango/views.py
+++ rango/views.py
@@ -4,6 +4,8 @@ from rango.models import Category, Page
 from rango.forms import CategoryForm, PageForm, UserForm, UserProfileForm
 from django.shortcuts import redirect
 from django.urls import reverse
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 def index(request):
 	# Query the database for a list of ALL categories currently stored.
@@ -43,6 +45,7 @@ def show_category(request, category_name_slug):
 	# Go render the response and return it to the client.
 	return render(request, 'rango/category.html', context=context_dict)
 
+@login_required
 def add_category(request):
 	form = CategoryForm()
 
@@ -60,6 +63,7 @@ def add_category(request):
 	#renders the form with error messages (if any)
 	return render(request, 'rango/add_category.html', {'form': form})
 
+@login_required
 def add_page(request, category_name_slug):
 	try:
 		category = Category.objects.get(slug=category_name_slug)
@@ -129,3 +133,38 @@ def register(request):
 							'profile_form' : profile_form,
 							'registered' : registered}
 				)
+
+def user_login(request):
+	# If request is a POST, login attempt has been made
+	if request.method == 'POST':
+		username = request.POST.get('username')
+		password = request.POST.get('password')
+
+ 		# Django inbuilt user/password validation
+		# returns User object if it is valid
+		user = authenticate(username=username, password=password)
+
+		if user: # true if authentication passed
+			if user.is_active: # i.e. is the user currently enabled?
+				login(request, user)
+				return redirect(reverse('rango:index'))
+
+			else:
+				return HttpResponse("Your Rango account is disabled.")
+		
+		else: # bad login details provided
+			print(f"Invalid login details: {username}, {password}")
+			return HttpResponse("Invalid login details supplied.")
+
+	# Default to treat request as a HTTP GET
+	else:
+		return render(request,'rango/login.html')
+
+@login_required
+def user_logout(request):
+	logout(request)
+	return redirect(reverse('rango:index'))
+
+@login_required
+def restricted(request):
+	return render(request, 'rango/restricted.html')
